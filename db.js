b4f771506ff9a5e68ec526b952f05de789b0814f
TysonPWilliams <tyson.williams95@gmail.com>
2025-06-10 20:49:47 +1000

    Added db.js

---
+++ db.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+
+//Connect to MongoDB
+export async function connect() {
+    await mongoose.connect(process.env.MONGODB_URI)
+    console.log(mongoose.connection.readyState == 1 ? "Connected to MongoDB" : "Failed to connect to MongoDB")
+}
+
+// Disconnect from MongoDB
+export async function disconnect() {
+    await mongoose.connection.close()
+    console.log(mongoose.connection.readyState == 0 ? "Disconnected from MongoDB" : "Failed to disconnect from MongoDB")
+}
+
+export default { connect, disconnect }
